6f43cdde4e2ef5b4745f797d325803327292543d
rosoarv <rosoarv@gmail.com>
2018-08-31 18:17:17 +0800

    added config

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+const port = process.env.PORT || config.get('port');
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use((err, req, res, next) => {
+    res.status(500).json({ code: 'Invalid Request', message: err.message });
+});
+
+app.listen(port, () => console.log(`Listening on ${port}`));
